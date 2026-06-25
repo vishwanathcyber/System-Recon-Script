aceb4b433d627915a160ce5501fc63103e8d7409
Vishwanath <vishwasharma300@gmail.com>
2026-06-26 03:35:51 +0530

    feat: add user information collector

---
+++ src/collectors/user.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# users.sh
+#
+# Module: User Information Collector
+#
+
+collect_users_info() {
+
+    echo "=============================="
+    echo "USER INFORMATION"
+    echo "=============================="
+
+    who
+
+    echo
+}
