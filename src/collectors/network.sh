b5ff4c8a6a1b33e792289e82b93d283c9d26680f
Vishwanath <vishwasharma300@gmail.com>
2026-06-26 03:59:15 +0530

    feat: enhance network collector with interface and routing information

--- src/collectors/network.sh
+++ src/collectors/network.sh
@@ -3,16 +3,48 @@
 #
 # network.sh
 #
-# Module: Network Information Collector
+# Network Information Collector
 #
 
 collect_network_info() {
 
-    echo "=============================="
-    echo "NETWORK INFORMATION"
-    echo "=============================="
+    echo "========================================"
+    echo "        NETWORK INFORMATION"
+    echo "========================================"
 
-    ip address
+    echo "Hostname:"
+    hostname
 
     echo
+
+    echo "IP Addresses:"
+    ip addr show
+
+    echo
+
+    echo "Default Gateway:"
+    ip route | grep default
+
+    echo
+
+    echo "DNS Servers:"
+    cat /etc/resolv.conf
+
+    echo
+
+    echo "Network Interfaces:"
+    ip link show
+
+    echo
+
+    echo "Listening Ports:"
+    ss -tuln
+
+    echo
+
+    echo "Routing Table:"
+    ip route
+
+    echo
+
 }
