73510a3c4d4a47c337a7fb6df535bfd6a2a5e988
Vishwanath <vishwasharma300@gmail.com>
2026-06-26 03:34:58 +0530

    feat: add network information collector

---
+++ src/collectors/network.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# network.sh
+#
+# Module: Network Information Collector
+#
+
+collect_network_info() {
+
+    echo "=============================="
+    echo "NETWORK INFORMATION"
+    echo "=============================="
+
+    ip address
+
+    echo
+}
