c578a26d78162d1b79581b3b616faad1b735fe40
Vishwanath <vishwasharma300@gmail.com>
2026-06-26 03:33:13 +0530

    feat: add memory information collector

---
+++ src/collectors/memory.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# memory.sh
+#
+# Module: Memory Information Collector
+#
+
+collect_memory_info() {
+
+    echo "=============================="
+    echo "MEMORY INFORMATION"
+    echo "=============================="
+
+    free -h
+
+    echo
+}
